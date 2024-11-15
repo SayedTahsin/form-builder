645a12dc1d4b404cc9606640bf27348c08579197
SayedTahsin <tahsin@appscode.com>
2024-11-15 18:24:04 +0600

    design fix for Horizontal-layout
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- src/components/layout/HorizonTal.vue
+++ src/components/layout/HorizonTal.vue
@@ -17,18 +17,20 @@ const { errCnt, updateErr } = useBlockValidity(props.parentErr)
 <template>
   <div v-if="ui.showLabels">
     <div class="is-flex is-justify-content-space-between p-4 bg-gray-400">
-      <p @click="show = !show">{{ ui.label }}</p>
-      <p>{{ errCnt }}</p>
-      <p>{{ ui.description }}</p>
+      <h6 @click="show = !show">{{ ui.label }}</h6>
+      <div>
+        <p>{{ errCnt }}</p>
+        <p>{{ ui.description }}</p>
+      </div>
     </div>
-    <div v-show="show" class="flex space-x-3 is-fullwidth">
+    <div v-show="show" class="is-flex gap-16 is-fullwidth">
       <template v-for="el in ui.elements" :key="el.label">
         <AllElement :el="el" :c-data="parentData" :parent-err="updateErr" />
       </template>
     </div>
   </div>
   <div v-else>
-    <div class="flex space-x-3 w-full">
+    <div class="is-flex gap-16 w-full">
       <AllElement
         v-for="el in ui.elements"
         :key="el.label"
